6c8da8d5cc9f2ef0dd92b5b70fa25aa648253c4e
s24729-pj <s24729@pjwstk.edu.pl>
2024-02-13 15:29:23 +0100

    Initial commit

---
+++ src/main/java/org/example/onlinestoremockup/OnlineStoreMockupApplication.java
@@ -0,0 +1,13 @@
+package org.example.onlinestoremockup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreMockupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreMockupApplication.class, args);
+	}
+
+}
