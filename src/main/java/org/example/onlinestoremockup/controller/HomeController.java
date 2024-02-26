e9d8d1e17585fe5bad4fe380774a1c1c104516fb
lukas <s24729@pjwstk.edu.pl>
2024-02-26 15:02:58 +0100

    All items show up on homepage

--- src/main/java/org/example/onlinestoremockup/controller/HomeController.java
+++ src/main/java/org/example/onlinestoremockup/controller/HomeController.java
@@ -1,14 +1,23 @@
 package org.example.onlinestoremockup.controller;
 
+import org.example.onlinestoremockup.service.ItemService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/onlinestoremockup")
 public class HomeController {
+    private final ItemService itemService;
+
+    public HomeController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
     @GetMapping
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("items", itemService.getAllItems());
         return "index";
     }
 }
